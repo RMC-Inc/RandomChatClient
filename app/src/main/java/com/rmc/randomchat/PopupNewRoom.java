99441dc073c6e5c063a8832a70f51771520900b6
Claudio-256 <cladotti31@gmail.com>
2022-01-25 18:18:40 +0100

    Modificata l'interfaccia di creazione di una stanza, tuttavia occorre inviare la nuova stanza al server per aggiungerla alle altre.

--- app/src/main/java/com/rmc/randomchat/PopupNewRoom.java
+++ app/src/main/java/com/rmc/randomchat/PopupNewRoom.java
@@ -6,51 +6,81 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.rmc.randomchat.entity.Room;
 import com.skydoves.colorpickerview.ColorEnvelope;
 import com.skydoves.colorpickerview.ColorPickerDialog;
 import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
 
+import mobi.upod.timedurationpicker.TimeDurationPicker;
+
 public class PopupNewRoom  {
 
     private Button createnewroom;
+    private Button buttontime;
     private Button buttoncolor;
+    //private TimeDurationPicker t_duration;
+    private EditText room_name;
+    private int color = 0;
+    private int time = 0;
 
     public void showPopupWindow(final View view) {
 
         LayoutInflater inflater = (LayoutInflater) view.getContext().getSystemService(view.getContext().LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.popupnewroom, null);
 
-        //Per rendere elementi inattivi al di fuori della finestra popup
         boolean focusable = true;
 
-        //creiamo una classe PopupWindow con i parametri personalizzati
         final PopupWindow popupWindow = new PopupWindow(popupView, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, focusable);
 
-        //settiamo il popup al centro dello schermo
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
 
+        room_name = popupView.findViewById(R.id.newroomname);
 
-        //Inizializzare tutti gli elementi da visualizzare nel popup
+        //t_duration = popupView.findViewById(R.id.timeDurationInput);
+
+        buttontime = popupView.findViewById(R.id.newroomtime);
+        buttontime.setOnClickListener(v -> {
+            time = (int) (Math.random() * 50);
+        });
+
+        /*buttontime.setOnClickListener(v -> {
+            buttontime.setVisibility(View.GONE);
+            buttoncolor.setVisibility(View.GONE);
+            t_duration.setVisibility(View.VISIBLE);
+            //todo vedere come chiuderlo
+        });*/
+
+
 
         createnewroom = popupView.findViewById(R.id.createnewroom);
         createnewroom.setOnClickListener(v -> {
-            //TODO INVIA COMANDO AL SERVER PER CREARE LA STANZA
+            if(color == 0)
+                Toast.makeText(v.getContext(),"Seleziona un colore per la stanza.",Toast.LENGTH_SHORT).show();
+            else
+                if((room_name.getText().toString()).trim().isEmpty())
+                    room_name.setError("Inserisci un nome.");
+                else if((room_name.getText().toString()).getBytes().length >30)
+                    room_name.setError("Il nome non deve superare i 30 caratteri.");
+                else{
+                    Room r = new Room(room_name.getText().toString(), time, color);
+                    //todo invia r al server
+                }
         });
-        //Clicca in una zona qualsiasi per chiudere il popup
 
-        Room room = new Room();
+        //Room room = new Room();
         buttoncolor = popupView.findViewById(R.id.buttoncolor);
         buttoncolor.setOnClickListener(view1 -> new ColorPickerDialog.Builder(view1.getContext())
                 .setTitle("Seleziona un colore")
                 .setPositiveButton(getString(R.string.confirm),
                         (ColorEnvelopeListener) (envelope, fromUser) -> {
                             setLayoutColor(envelope);
-                            room.setRoomColor(envelope.getColor());
-
+                            color = envelope.getColor();
                         })
                 .setNegativeButton(getString(R.string.cancel),
                         (dialogInterface, i) -> dialogInterface.dismiss())
@@ -60,14 +90,10 @@ public class PopupNewRoom  {
                 .show());
 
 
-        popupView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                //Chiude la finestra quando si clicca a caso
-                popupWindow.dismiss();
-                return true;
-            }
+        popupView.setOnTouchListener((v, event) -> {
+            v.performClick();
+            popupWindow.dismiss();
+            return true;
         });
 
     }
